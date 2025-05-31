da393f40dc1c3e216a9aa0d717fd6390c83796f9
Mark Calla <markcalla@byui.edu>
2025-05-31 17:51:59 -0500

    now i think i found the error

--- routes/index.ts
+++ routes/index.ts
@@ -5,7 +5,7 @@ import booksRoutes from './booksRoutes';
 const router = express.Router();
 
 // Mount swagger routes at '/'
-router.use('/', swaggerRoutes);
+router.use('/api-docs', swaggerRoutes);
 
 // Mount contacts routes at '/contacts'
 router.use('/books', booksRoutes);
